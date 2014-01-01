21e86a88e08f3ccd6c5b8d5e70175c4901ec22f7
Ubuntu <ubuntu@ip-10-0-0-227.us-west-1.compute.internal>
2014-01-01 08:25:11 +0000

    more logic for deciding how much memory to give to kafka

--- libraries/jvm_opts_helper.rb
+++ libraries/jvm_opts_helper.rb
@@ -2,9 +2,14 @@ module JvmOptsHelper
   def self.gen_heap_opts(total_mem_str)
     if total_mem_str  =~ /(\d+)/
       total_mem = $1.to_i
-    
-      # We're reserving 256m to run zookeeper and another 256m for jmxtrans 
-      mem = total_mem - 256 * 1024 - 256 * 1024 
+
+      divided_mem = total_mem / 3
+
+      # others = zookeeper and jmxtrans. Max mem we'll give them is 256MB
+      upper_bound_mem_for_others = 2 * 256 * 1024    
+      mem_for_others = (divided_mem * 2 < upper_bound_mem_for_others)? divided_mem * 2 : upper_bound_mem_for_others
+
+      mem = total_mem - mem_for_others
       return "-Xmx#{mem}k -Xms#{mem}k"
     end
   end
