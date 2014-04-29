d978faedec58484a7c54ae3ff0c4d33e7d69aed5
Ubuntu <ubuntu@ip-10-0-0-227.us-west-1.compute.internal>
2014-04-29 05:58:19 +0000

    actually use chef to install kafka now

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,16 +7,53 @@
 # All rights reserved - Do Not Redistribute
 #
 
+case node[:platform]
+  when "redhat","centos","fedora","scientific", "amazon"
+  remote_file "#{Chef::Config[:file_cache_path]}/kafka.rpm" do 
+    not_if "rpm -qa | grep -qx 'kafka'"
+    source node["kafka"]["rpm_url"]
+    notifies :install, "rpm_package[kafka]", :immediately
+  end
+
+  rpm_package "kafka" do
+    source "#{Chef::Config[:file_cache_path]}/kafka.rpm" 
+    only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/kafka.rpm")} 
+    action :nothing
+  end
+
+#  package "kafka" do
+#    action :install
+#    source node["kafka"]["rpm_url"]
+#    provider Chef::Provider::Package::Rpm
+#    not_if "rpm -qa | grep -qx 'kafka'"
+#  end
+end
+
 template "/opt/kafka/bin/kafka-server-start.sh" do
   source "kafka-server-start.sh.erb"
   mode  "0755"
   variables({
      :heap_opts => JvmOptsHelper.gen_heap_opts(node[:memory][:total])
   })
-  notifies :restart, "service[kafka]"
+end
+
+template "/etc/init.d/kafka-mirror" do
+  source "kafka-mirror.erb"
+  mode  "0755"
 end
 
 service "kafka" do
   supports :restart => true, :status => true, :reload => true
-  action [:enable, :start]
+  action [:enable]
+end
+
+service "kafka-mirror" do
+  supports :restart => true, :status => true, :reload => true
+  action :nothing
+end
+
+directory "/var/log/kafka" do
+  owner "kafka"
+  group "kafka"
+  action :create
 end
